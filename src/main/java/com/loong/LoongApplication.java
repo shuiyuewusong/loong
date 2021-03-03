711b432822b876ead0de9c99faf294fda9e61960
水月无嵩 <964377809@qq.com>
2021-03-03 11:28:23 +0800

    Initial commit

---
+++ src/main/java/com/loong/LoongApplication.java
@@ -0,0 +1,13 @@
+package com.loong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoongApplication.class, args);
+    }
+
+}
